0644bb2e35d94436525b6984b1ff919e308361b4
0mpurdy <0mpurdy@gmail.com>
2016-10-08 01:08:29 +0100

    Added README
    Also blank .npmignore file to allow correct npm installation
    Added /index to barrel references to be properly located by systemjs

---
+++ README.md
@@ -0,0 +1,6 @@
+Lite-Module
+===========
+
+The purpose of this module was to try and create the bare minimum module in angular 2.
+
+It is incorporated in the `lite-app` repository again for testing.
--- src/lite-module.module.ts
+++ src/lite-module.module.ts
@@ -2,7 +2,7 @@ import { NgModule }           from '@angular/core';
 import { CommonModule }       from '@angular/common';
 import { FormsModule }        from '@angular/forms';
 
-import { LiteComponent } from './components';
+import { LiteComponent } from './components/index';
 
 @NgModule({
   imports:      [ CommonModule, FormsModule ],
